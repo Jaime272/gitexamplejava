273246785fd5133e436592be16eab5f7eeed612d
John Doe <johndoe@example.com>
2020-09-21 10:46:38 +0900

    디벨롭2 세번째 코드

--- GitExampleJava/src/App.java
+++ GitExampleJava/src/App.java
@@ -4,6 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World1");
 		System.out.println("Hello World2");
+		System.out.println("Hello World3-devlop2");
+		
 	}
 
 }
