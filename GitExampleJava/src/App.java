1a9b03e2f41bdb7f567acbc2bd29631d6dff0af1
John Doe <johndoe@example.com>
2020-09-21 10:44:34 +0900

    세번째 코드2

--- GitExampleJava/src/App.java
+++ GitExampleJava/src/App.java
@@ -4,6 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World1");
 		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 
 }
